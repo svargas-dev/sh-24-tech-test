33dfe78b1d15e19cdea4ce931312cc44b990b07d
Sam Vargas <sam.vargas@posteo.com>
2025-05-25 10:41:22 +0100

    Refactor actions validation, add caching, & comments

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,35 +1,45 @@
 "use server";
 
 import { redirect } from 'next/navigation'
-import { z } from "zod";
+import { z } from "zod/v4";
 
 export async function checkPostcode(prevState: {
   message: string;
 }, formData: FormData) {
   if (!formData.get("postcode")) {
-    return { 
+    return {
       message: "Postcode is required"
     };
   }
 
-  const schema = z.object({
-    postcode: z.string().trim().toUpperCase().regex(/^[A-Z\d]+ ?[A-Z\d]+$/),
+  const PostCodeSchema = z.object({
+    // This is very simple normalization & validation of a postcode input
+    // In the real-world you may want a more complex regex validation
+    // or a library to validate postcode formats
+    postcode: z.string()
+      .trim()
+      .toUpperCase()
+      .regex(/^[A-Z\d]+ ?[A-Z\d]+$/)
+      .transform(val => val.replace(/\s/g, "")),
   });
-  const parse = schema.safeParse({
-    postcode: (formData.get("postcode") as string).replace(/\s/g, ""),
+  const parse = PostCodeSchema.safeParse({
+    postcode: formData.get("postcode"),
   });
 
   if (!parse.success) {
-    return { 
+    return {
       message: "Your postcode must be a valid UK postcode."
     };
   }
 
-  if (allowlist.includes(parse.data.postcode)) {
+  if (allowList.includes(parse.data.postcode)) {
     redirect("/success");
   }
 
-  const response = await fetch(`http://postcodes.io/postcodes/${parse.data.postcode}`);
+  const response = await fetch(`http://postcodes.io/postcodes/${parse.data.postcode}`, {
+    cache: "force-cache",
+    next: { revalidate: 60 * 60 }
+  });
   const data = await response.json();
 
   if (!data.result) {
@@ -46,7 +56,7 @@ export async function checkPostcode(prevState: {
 }
 
 const lsoaRegex = /^Lambeth|Southwark/;
-const allowlist = [
+const allowList = [
   "SH241AA",
   "SH241AB",
 ];
