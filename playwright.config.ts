5dd85177916b5106a45779e8ea156c49f61d5741
Sam Vargas <sam.vargas@posteo.com>
2025-05-25 18:43:14 +0100

    Add Playwright E2E tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig, devices } from '@playwright/test';
 
+const PORT = process.env.PORT || 3000;
+const BASE_URL = process.env.BASE_URL || `http://localhost:${PORT}`;
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
@@ -26,7 +28,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: BASE_URL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
---
+++ tests/postcode.spec.ts
@@ -0,0 +1,98 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('/');
+
+  await expect(page).toHaveTitle(/Sam Vargas | SH:24 Tech Test - May 2025/);
+});
+
+test('should show error message when postcode is empty', async ({ page }) => {
+  await page.goto('/');
+
+  await page.getByRole('button', { name: 'Continue' }).click();
+
+  await expect(page.getByRole('status')).toHaveText(/Postcode is required/);
+});
+
+test('should show error message when postcode is invalid', async ({ page }) => {
+  await page.goto('/');
+
+  await page.getByRole('textbox', { name: 'Postcode' }).fill('invalid postcode');
+  await page.getByRole('button', { name: 'Continue' }).click();
+
+  await expect(page.getByRole('status')).toHaveText(/Your postcode must be a valid UK postcode./);
+});
+
+test('should disable button when postcode is valid & submitting form', async ({ page }) => {
+  await page.goto('/');
+
+  await page.getByRole('textbox', { name: 'Postcode' }).fill('SH241AA');
+  await page.getByRole('button', { name: 'Continue' }).click();
+
+  await expect(page.getByRole('button', { name: 'Continue' })).toBeDisabled();
+});
+
+test('should redirect to success page when postcode is valid (allow list)', async ({ page }) => {
+  await page.goto('/');
+
+  await page.getByRole('textbox', { name: 'Postcode' }).fill('SH241AA');
+  await page.getByRole('button', { name: 'Continue' }).click();
+
+  await expect(page).toHaveURL('/success');
+
+  await expect(page).toHaveTitle(/Sam Vargas \| Available in your area/);
+  const description = await page.locator('meta[name="description"]').getAttribute('content');
+  expect(description).toBe('Sam Vargas tech test for SH:24 for hiring a new software engineer in May/June 2025');
+  await expect(page.getByRole("heading", { name: "We're available in your area!" })).toBeVisible();
+});
+
+test('should redirect to success page when postcode is valid (postcode.io)', async ({ page }) => {
+  await page.goto('/');
+
+  await page.getByRole('textbox', { name: 'Postcode' }).fill('SE5 9QY');
+  await page.getByRole('button', { name: 'Continue' }).click();
+
+  await expect(page).toHaveURL('/success');
+});
+
+test('should redirect to out of service area page when postcode is valid (postcode.io) & not in service area', async ({ page }) => {
+  await page.goto('/');
+
+  await page.getByRole('textbox', { name: 'Postcode' }).fill('W5 4SA');
+  await page.getByRole('button', { name: 'Continue' }).click();
+
+  await expect(page).toHaveURL('/out-of-service-area');
+
+  await expect(page).toHaveTitle(/Sam Vargas \| Out of service area/);
+  const description = await page.locator('meta[name="description"]').getAttribute('content');
+  expect(description).toBe('Sam Vargas tech test for SH:24 for hiring a new software engineer in May/June 2025');
+  await expect(page.getByRole("heading", { name: "We're sorry, we don't offer services to that area." })).toBeVisible();
+});
+
+test('should show error when no postcode is entered, then show success page when a valid postcode is entered', async ({ page }) => {
+  await page.goto('/');
+
+  await page.getByRole('button', { name: 'Continue' }).click();
+
+  await expect(page.getByRole('status')).toHaveText(/Postcode is required/);
+
+  await page.getByRole('textbox', { name: 'Postcode' }).fill('SH241AA');
+  await page.getByRole('button', { name: 'Continue' }).click();
+
+  await expect(page).toHaveURL('/success');
+});
+
+
+test('should show error when postcode is invalid, then show success page when a valid postcode is entered', async ({ page }) => {
+  await page.goto('/');
+
+  await page.getByRole('textbox', { name: 'Postcode' }).fill('nonesense');
+  await page.getByRole('button', { name: 'Continue' }).click();
+
+  await expect(page.getByRole('status')).toHaveText(/Your postcode must be a valid UK postcode./);
+
+  await page.getByRole('textbox', { name: 'Postcode' }).fill('SH241AA');
+  await page.getByRole('button', { name: 'Continue' }).click();
+
+  await expect(page).toHaveURL('/success');
+});
